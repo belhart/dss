d9252fae50946ed55e9cfb8915e96df990df617a
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-07-15 16:51:15 +0200

    removed comments

--- DSS/index.aspx.cs
+++ DSS/index.aspx.cs
@@ -18,28 +18,19 @@ namespace DSS
         }
         protected void ImportCSV(object sender, EventArgs e)
         {
-            //Upload and save the file
             string csvPath = Server.MapPath("~/Files/") + Path.GetFileName(FileUpload1.PostedFile.FileName);
             FileUpload1.SaveAs(csvPath);
-
-            //Create a DataTable.
             DataTable dt = new DataTable();
             dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id", typeof(int)),
             new DataColumn("Name", typeof(string)),
             new DataColumn("Country",typeof(string)) });
-
-            //Read the contents of CSV file.
             string csvData = File.ReadAllText(csvPath);
-
-            //Execute a loop over the rows.
             foreach (string row in csvData.Split('\n'))
             {
                 if (!string.IsNullOrEmpty(row))
                 {
                     dt.Rows.Add();
                     int i = 0;
-
-                    //Execute a loop over the columns.
                     foreach (string cell in row.Split(','))
                     {
                         dt.Rows[dt.Rows.Count - 1][i] = cell;
