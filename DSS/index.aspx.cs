fa903945753af3e4be94df5edef9551674ff4ca1
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-07-15 16:44:02 +0200

    basic uploader added

---
+++ DSS/index.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Data;
+
+namespace DSS
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void ImportCSV(object sender, EventArgs e)
+        {
+            //Upload and save the file
+            string csvPath = Server.MapPath("~/Files/") + Path.GetFileName(FileUpload1.PostedFile.FileName);
+            FileUpload1.SaveAs(csvPath);
+
+            //Create a DataTable.
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id", typeof(int)),
+            new DataColumn("Name", typeof(string)),
+            new DataColumn("Country",typeof(string)) });
+
+            //Read the contents of CSV file.
+            string csvData = File.ReadAllText(csvPath);
+
+            //Execute a loop over the rows.
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+
+                    //Execute a loop over the columns.
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+
+            //Bind the DataTable.
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+    }
+}
