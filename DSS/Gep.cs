f77d8cf6847df830d99bbe4758afa6050f9acd3b
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-07-16 19:56:18 +0200

    classes moved

---
+++ DSS/Gep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dssgyak
+{
+    class Gep
+    {
+        public int hanyadikmenet = 1;
+
+        // mikor fejezte be a termékeket
+        public List<int> naplo;
+        // mennyi idő alatt csinál 1et
+        public int ido;
+
+
+        public Gep(int ido)
+        {
+            naplo = new List<int>();
+            this.ido = ido;
+        }
+    }
+}
